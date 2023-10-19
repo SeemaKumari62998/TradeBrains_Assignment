03d20fa938a72f8baba878feec17e2119c89c1b6
SeemaKumari <seemakumari62998@gmail.com>
2023-10-19 15:10:44 +0530

    Fixed deployment issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 import "./Header.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  const navigate = useNavigate();
-
-  const navigator = () => {
-    navigate("/");
-  };
-
   return (
     <div className="header">
       <div>
